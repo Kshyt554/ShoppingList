0248f5a227272b6b3ab18c0544f79f919c79bf0b
Kshyt554 <limvova2003@gmail.com>
2023-12-10 20:12:53 +0100

    Update Hw5
    Hw5

--- client/src/components/navbar.js
+++ client/src/components/navbar.js
@@ -1,10 +1,15 @@
+import './navbar.css';
 import {Link} from "react-router-dom";
 import {useCookies} from "react-cookie";
 import {useNavigate} from "react-router-dom";
+import React from 'react';
+import {useTranslation} from 'react-i18next';
 
 export const Navbar = () => {
     const [cookies, setCookies] = useCookies(["access_token"]);
     const navigate = useNavigate();
+    const {i18n} = useTranslation();
+    const {t} = useTranslation();
 
     const logout = () => {
         setCookies("access_token", "");
@@ -12,17 +17,22 @@ export const Navbar = () => {
         navigate("/authorization");
     }
 
+    const changeLanguage = (lng) => {
+        i18n.changeLanguage(lng);
+    };
+
     return (
         <div className="navbar">
-            
-            <Link to="/"> Home </Link>
-            <Link to="/create-list"> Create List </Link>
+            <button onClick={() => changeLanguage('en')} className="eng">English</button>
+            <button onClick={() => changeLanguage('cs')} className="cz">Čeština</button>
+            <Link to="/"> {t('Home')} </Link>
+            <Link to="/create-list"> {t('Create List')} </Link>
             {!cookies.access_token ? (
-                <Link to="/authorization"> Authorization </Link>
+                <Link to="/authorization"> {t('Authorization')} </Link>
             ) : (
                 <>
-                <Link to="/lists"> Archive </Link>
-                <button onClick={logout}> Logout </button>
+                <Link to="/lists"> {t('Archive')} </Link>
+                <button onClick={logout}> {t('Logout')} </button>
                 </>
             )}            
         </div>
